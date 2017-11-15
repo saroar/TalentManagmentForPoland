d84f723ecd03fcf7eb8f469f48cc9b96970999a9
saroar <saroar9@gmail.com>
2017-11-15 15:32:01 +0300

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def authenticate_active_admin_user!
-  #   authenticate_user!
-  #   unless current_user.has_role? :admin
-  #     flash[:notice] = 'Sorry you are not Authorized to access this resource!'
-  #     redirect_to root_path
-  #   end
-  # end
-  #
-  def after_sign_in_path_for(resource)
-    if current_user.has_role?(:admin, :talentcouch, :consultant)
-      admin_dashboard_path
-    else
-      root_path
+  def authenticate_active_admin_user!
+    authenticate_user!
+
+    [:admin, :talentcouch, :consultant].each do |role|
+      unless current_user.has_role? role
+        flash[:notice] = 'Sorry you are not Authorized to access this resource!'
+        redirect_to root_path
+      end
     end
+
+  end
+
+  def after_sign_in_path_for(resource)
+    [:admin, :talentcouch, :consultant].each do |role|
+      current_user.has_role? role
+      admin_dashboard_path
+    end
+
+    root_path
   end
 
   protected
