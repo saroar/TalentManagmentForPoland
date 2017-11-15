8d0163c7e8c1445ce2226affed1a73dbb74389dc
saroar <saroar9@gmail.com>
2017-11-15 16:43:37 +0300

    update bin

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -24,5 +24,6 @@
   if Rails.env.staging? || Rails.env.production?
     WickedPdf.config = { :exe_path => "#{Rails.root}/bin/wkhtmltopdf" }
   else
+    binding.pry
     WickedPdf.config = { exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf') }
   end
