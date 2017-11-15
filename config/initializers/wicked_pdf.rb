b535712eaebd727b86d44d8757c3b20b513f18bc
saroar <saroar9@gmail.com>
2017-11-15 16:05:43 +0300

    update WickedPdf.config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -21,11 +21,17 @@
 #
 #   # exe_path: '/usr/local/bin/wkhtmltopdf'
 # }
-  if Rails.env.staging? || Rails.env.production?
-    WickedPdf.config = {
-      :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
-    }
-  else
-    WickedPdf.config = { exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf') }
-  end
+#   if Rails.env.staging? || Rails.env.production?
+#     WickedPdf.config = {
+#       :exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+#     }
+#   else
+#     WickedPdf.config = { exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf') }
+#   end
 
+
+  WickedPdf.config = {
+    #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+    #:layout => "pdf.html",
+    :exe_path => '/usr/local/bin/wkhtmltopdf'
+  }
