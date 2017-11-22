de8fd663b8cb520fbea73a45f44ceb3f638233f1
saroar <saroar9@gmail.com>
2017-11-22 12:24:12 +0300

    add more status

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -1,5 +1,5 @@
 class Talent < ApplicationRecord
-  enum status: [ :pending, :start, :done ]
+  enum status: [ :pending, :processing, :start, :done, :refuse ]
   mount_uploader :passport_copy, ImageUploader
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   validates :given_name, :surname, :place_of_birth, :date_of_birth, :date_of_expiry, :passport_copy, :starting_payment, presence: true
